f507d7ca6d94e34e8cb7357b8c7bbf6426fa4911
Kolt-c <ts.nicolas@outlook.com>
2026-02-01 15:59:07 +0200

    adds task 0142

---
+++ src/main/java/task/task01/task0142/Solution0142.java
@@ -0,0 +1,11 @@
+package task.task01.task0142;
+
+/*
+Амиго очень умный
+*/
+
+public class Solution0142 {
+    public static void main(String[] args) {
+        System.out.println("Амиго очень умный");
+    }
+}
