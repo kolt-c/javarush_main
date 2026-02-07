6da63bc41a5443d33f9dce350556b403903bdad9
Kolt-c <ts.nicolas@outlook.com>
2026-02-07 14:11:28 +0200

    adds task 0114

---
+++ src/main/java/task/task01/task0114/Solution.java
@@ -0,0 +1,18 @@
+package task.task01.task0114;
+/*
+JavaRush. Learn once - use anywhere
+*/
+public class Solution {
+    static void main() {
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+        System.out.println("JavaRush. Learn once - use anywhere");
+    }
+}
