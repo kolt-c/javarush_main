8b66d0d7abf7f48eb8484fcb423cff447ce8b6e5
Kolt-c <ts.nicolas@outlook.com>
2026-02-07 13:44:33 +0200

    adds task 0110

---
+++ src/main/java/task/task01/task0110/Solution.java
@@ -0,0 +1,10 @@
+package task.task01.task0110;
+/*
+На нашем экране — переменная
+*/
+public class Solution {
+    static void main() {
+        String name = "qwerty";
+        System.out.println(name);
+    }
+}
