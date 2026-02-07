ab8daa6ea0cf984bf9241dce27b558bef50f697c
Kolt-c <ts.nicolas@outlook.com>
2026-02-07 12:29:45 +0200

    adds task 0105

---
+++ src/main/java/task/task01/task0105/Solution.java
@@ -0,0 +1,11 @@
+package task.task01.task0105;
+/*
+Объявляем переменные
+*/
+public class Solution {
+    static void main() {
+        String name;
+        int age;
+        String city;
+    }
+}
