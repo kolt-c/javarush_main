0467b87d1e77908fffd5ec72ef17f95a0a429bb0
Kolt-c <ts.nicolas@outlook.com>
2026-02-07 13:47:00 +0200

    adds task 0111

---
+++ src/main/java/task/task01/task0111/Solution.java
@@ -0,0 +1,11 @@
+package task.task01.task0111;
+/*
+Одной переменной недостаточно
+*/
+public class Solution {
+    static void main() {
+        int a = 10;
+        int b = -5;
+        String s = "sdf";
+    }
+}
