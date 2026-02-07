86a8ad630266696fefd2fcf111d4aa9bf70baaec
Kolt-c <ts.nicolas@outlook.com>
2026-02-07 12:25:54 +0200

    adds tasks 0101 0102 0103

---
+++ src/main/java/task/task01/task0101/Solution0101.java
@@ -0,0 +1,9 @@
+package task.task01.task0101;
+/*
+Я думаю, быть программистом круто
+*/
+public class Solution0101 {
+    static void main() {
+        System.out.println("Я думаю, быть программистом круто");
+    }
+}
---
+++ src/main/java/task/task01/task0102/Solution.java
@@ -0,0 +1,14 @@
+package task.task01.task0102;
+/*
+Комплимент учителю
+*/
+public class Solution {
+    static void main() {
+        String text = "Элли самая красивая";
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+    }
+}
