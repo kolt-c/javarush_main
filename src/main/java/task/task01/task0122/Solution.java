60c4684790b78b2484247251d06ae7a2349c8735
Kolt-c <ts.nicolas@outlook.com>
2026-05-24 00:43:55 +0300

    adds task 0122

---
+++ src/main/java/task/task01/task0122/Solution.java
@@ -0,0 +1,14 @@
+package task.task01.task0122;
+
+/*
+Исправь ошибку
+*/
+
+public class Solution {
+    static void main() {
+            int a = 56;
+            int b = 30;
+            int c = 14;
+            System.out.print(a + b + c);
+    }
+}
